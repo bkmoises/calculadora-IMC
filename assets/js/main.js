d7bbc59f70f4310fd33a04c8077dd5d818913aa4
moisesreis <moises.andrade@brlink.com.br>
2023-01-13 11:46:51 -0300

    correção de espaçamentos e pontuações

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const inWeight = event.target.querySelector('.weight');
   const inHeight = event.target.querySelector('.height');
-
   const weight = Number(inWeight.value);
   const height = Number(inHeight.value);
 
@@ -21,19 +20,17 @@ form.addEventListener('submit', function(event){
 
   const bmi = getBmi(weight, height);
   const bmiIndex = getBmiIndex(bmi);
-
   const message = `Your BMI is ${bmi} (${bmiIndex})`
 
   printResult(message, true);
 });
 
-function getBmi (weight, height) {
+function getBmi(weight, height) {
   const bmi = weight / height ** 2;
   return bmi.toFixed(2);
 };
 
-function getBmiIndex (bmi) {
-
+function getBmiIndex(bmi) {
   const index = [
     "Underweight",
     "Normal",
@@ -41,7 +38,6 @@ function getBmiIndex (bmi) {
     "Obesity",
     "Severe Obsity"
   ];
-
   if (bmi >= 39.9) return index[4];
   if (bmi >= 29.9) return index[3];
   if (bmi >= 24.9) return index[2];
@@ -54,11 +50,11 @@ function pCreator() {
   return p;
 };
 
-function printResult (message, valid) {
+function printResult(message, valid) {
   const result = document.querySelector('.result');
+  const p = pCreator();
   result.innerHTML = '';
 
-  const p = pCreator();
 
   if (valid) {
     p.classList.add('goodResult');
